15a44178bfefcf8621eca406755e7af6bc7ec01f
Kevin Scheffler <schefk2122@gmail.com>
2022-12-03 18:02:44 -0500

    started boilerplate for js-todoList

---
+++ script.js
@@ -0,0 +1,31 @@
+let todo1 = 'Get groceries';
+let todo2 = 'Wash car';
+let todo3 = 'Make dinner';
+
+let todos = ['Get groceries', 'Wash car', 'Make dinner'];
+todos.push('another todo');
+
+todos.pop();
+
+
+function addTodo(todoTitle) {
+  let element = document.createElement('div');
+  element.innerText = todoTitle;
+  document.body.appendChild(element);
+}
+
+addTodo(todo1);
+addTodo(todo2);
+addTodo(todo3);
+
+console.log(todos);
+
+
+
+
+
+
+
+
+
+
