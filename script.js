334799cb05243d9b0f8eb1ead976b789822b5124
Kevin Scheffler <schefk2122@gmail.com>
2022-12-04 18:28:52 -0500

    completed some practice exercises

--- script.js
+++ script.js
@@ -9,6 +9,42 @@ todos.forEach(function(todoTitle) {
 
 
 
+function toUpper(arr) {
+  let newArr = [];
+  arr.forEach(function(item) {
+    newArr.push(item.toUpperCase());
+  })
+  console.log(newArr);
+}
+
+toUpper(['hello', 'world']);
+
+function arrayDouble(arr) {
+  let newArr = [];
+  
+  arr.forEach(function(item) {
+    newArr.push(item);
+    newArr.push(item);
+  })
+  console.log(newArr);
+}
+
+arrayDouble(['bark', 'meow']);
+
+
+function arraySum(numsArr) {
+  let total = 0;
+  numsArr.forEach(function (nums) {
+    total += nums
+  })
+  console.log(total)
+}
+
+arraySum([1,2,3]);
+arraySum([5, -2, 7, 0]);
+
+
+
 
 
 
